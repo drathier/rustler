aa2686e54254c4fa7b10e5becf30c4bd0c07e268
Filip Haglund <drathier@users.noreply.github.com>
2021-10-11 19:15:54 +0200

    Update compiler.ex

--- rustler_mix/lib/rustler/compiler.ex
+++ rustler_mix/lib/rustler/compiler.ex
@@ -33,9 +33,10 @@ defmodule Rustler.Compiler do
 
       case compile_result do
         {_, 0} -> :ok
-        {asd, code} -> 
-          IO.inspect(asd)
-          raise "Rust NIF compile error (rustc exit code #{code}) #{inspect asd}"
+        {asd, code} ->
+          asds = Enum.join("", asd)
+          IO.inspect(asds)
+          raise "Rust NIF compile error (rustc exit code #{code}) #{asds}"
       end
 
       handle_artifacts(crate_full_path, config)
